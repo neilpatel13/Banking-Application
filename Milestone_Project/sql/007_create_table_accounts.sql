188f9c7245f7d94919d61e81cf20a35715726fcc
neilpatel13 <np656@njit.edu>
2023-05-02 00:35:23 -0400

    got tables done

---
+++ Milestone_Project/sql/007_create_table_accounts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS IS601_Accounts(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    account_number varchar(12) unique,
+    account_type varchar(15),
+    user_id int,
+    balance int DEFAULT 0,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES IS601_Users(id),
+    check((id > 0 && balance >= 0) || id == -1)),
+);
